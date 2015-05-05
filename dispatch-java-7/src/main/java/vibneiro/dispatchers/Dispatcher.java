0938354b1dc25ffd491c308472a1f1901e2d034d
Ivan Voroshilin <vibneiro@gmail.com>
2015-05-05 22:58:22 +0300

    Update Dispatcher.java

--- dispatch-java-7/src/main/java/vibneiro/dispatchers/Dispatcher.java
+++ dispatch-java-7/src/main/java/vibneiro/dispatchers/Dispatcher.java
@@ -2,15 +2,6 @@ package vibneiro.dispatchers;
 
 import javax.annotation.concurrent.ThreadSafe;
 
-/**
- * IDispatcher receives tasks and assigns to underlying worker thread.
- * There is a conceptual difference with java ExecutorService. In IDispatcher each task has corresponding dispatchId.
- * If IDispatcher receives task_2 with dispatchId and task_1 with the same dispatchId, was received earlier and still in progress
- * task_2 will be put in waiting state (i.e. in state queue, depends on implementation). Task_2 will be proceed only after
- * completion of task_1.<br>
- * <p></p>
- * This feature gives us ability to write simple not thread safe component and still get benefits of multi threading.
- */
 @ThreadSafe
 public interface Dispatcher {
 
